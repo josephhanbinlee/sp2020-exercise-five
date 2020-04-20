d58bcd78f33ce42b59a6ddecfc990da18f8529b1
Joseph <jhl843@nyu.edu>
2020-04-20 22:39:12 +0900

    new routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import './App.css';
+import {Route, BrowserRouter as Router, Redirect} from "react-router-dom";
+
+// Pages
+import Login from './containers/Login';
+import CreateAccount from './containers/CreateAccount';
+import UserProfile from "./containers/UserProfile";
 
 function App() {
-  return <div className="App"> </div>;
+  return <div className="App">
+    <Router>
+      <Route exact path="/">
+        <UserProfile />
+      </Route>
+      <Route exact path="/login">
+        <Login />
+      </Route>
+      <Route exact path="/create-account">
+        <CreateAccount />
+      </Route>
+    </Router>
+  </div>;
 }
 
 export default App;
