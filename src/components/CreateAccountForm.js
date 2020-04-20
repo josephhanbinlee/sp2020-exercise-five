4602784660277be932fc0d5b51ed54d47e5df60e
Joseph <jhl843@nyu.edu>
2020-04-20 22:08:45 +0900

    clean

---
+++ src/components/CreateAccountForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CreateAccountForm() {
+    return (
+        <div>
+            <p>Create Account Form</p>
+        </div>
+    );
+}
+
+export default CreateAccountForm;
